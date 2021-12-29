8a2aea34afc874f00b740e9e3ee32f759d8eae95
Eli Block <3347571+eliblock@users.noreply.github.com>
2021-12-28 19:19:21 -0800

    do not alert on missing identity when using sk 📵
    Potential `IdentityFile`s from an entry in `~/.ssh/config` are tried
    sequentially, and if a key file is missing a `no such identity` output is
    printed.
    
    During ssh key setup: maintain separate names for `-sk` vs. non-`-sk`
    keys, but symlink the non-`-sk` name to the `-sk` name if a
    YubiKey-backed key is created.
    
    Relying on the symlink, simplify `~/.ssh/config` such that only one
    `IdentityFile` is checked for GitHub, eliminating the error when a key
    is missing.

--- ssh/setup.sh
+++ ssh/setup.sh
@@ -10,52 +10,65 @@ source ../system_checks.sh
 echo '~~~~~~~~~~~ ~~~~~~~~~~ configuring ssh for GitHub access ~~~~~~~~~~ ~~~~~~~~~~~'
 
 github_noreply_email="3347571+eliblock@users.noreply.github.com"
-make_sk_key=false
-key_file_name="$HOME/.ssh/id_ed25519_github"
+
+# When a yubikey is present, store the key in a file indicating that it's an
+# ed25519-sk not an ed25519.
+key_name="$HOME/.ssh/id_ed25519_github"
+key_file_name="$key_name"
 if command_available ykman; then
   if [ "$(ykman list | wc -l)" -gt 0 ]; then
-    make_sk_key=true
     key_file_name="$HOME/.ssh/id_ed25519_sk_github"
     echo "YubiKey detected. Will create a YubiKey-backed key."
   fi
 fi
 
+# Do not attempt to overwrite keys - force manual intervention instead
 if [ -f "$key_file_name" ]; then
   echo "🛑 A key already exists at $key_file_name. Clean up and try again."
   exit 1
 fi
 
-read -rp "✨ Using ssh-keygen to create a key ($key_file_name). Follow the prompts. Ready to continue? [y/N]: " CONTINUE
+if [ -f "$key_name" ]; then
+  echo "🛑 A key already exists at $key_name. Clean up and try again."
+  exit 1
+fi
+
+echo "✨ Using ssh-keygen to create a key ($key_file_name). Follow the prompts."
+read -rp "Ready to continue? [y/N]: " CONTINUE
 if ! [[ "$CONTINUE" =~ ^[Yy]$ ]]; then
   echo "🛑 Okay! Try again when you're ready."
   exit 0
 fi
 unset CONTINUE
 
-if "$make_sk_key"; then
+# If we changed the filename, create a -sk key
+if [ "$key_name" != "$key_file_name" ]; then
   # https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key-for-a-hardware-security-key
   ssh-keygen -t ed25519-sk -C "$github_noreply_email" -f "$key_file_name"
+  ln -s "$key_file_name" "$key_name"
+  ln -s "$key_file_name.pub" "$key_name.pub"
 else
   # https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
   ssh-keygen -t ed25519 -C "$github_noreply_email" -f "$key_file_name"
 fi
 
-read -rp "✨ A key was generated! Adding it to ssh-agent (you will be prompted for the password you set). Ready to continue? [y/N]: " CONTINUE
+echo "✨ A key was generated! Adding it to ssh-agent (you may be prompted for the password you set)."
+read -rp "Ready to continue? [y/N]: " CONTINUE
 if ! [[ "$CONTINUE" =~ ^[Yy]$ ]]; then
   echo "🛑 Okay! Try again when you're ready."
   exit 0
 fi
 unset CONTINUE
 eval "$(ssh-agent -s)" 1> /dev/null
-ssh-add "$key_file_name"
+ssh-add "$key_name"
 
 cat << GITHUB_STEPS
-⚠️  manual action required ⚠️
 
-A key was successfully created ($key_file_name) but manual steps are required to
+⚠️  MANUAL ACTION REQUIRED ⚠️
+A key was successfully created ($key_name) but manual steps are required to
 complete setup:
 * copy the public key to your clipboard
-  pbcopy < $key_file_name.pub
+  pbcopy < $key_name.pub
 * add a new ssh key to your GitHub account. Be sure to give the key a meaningful
   name (e.g., $(hostname), or some other identifier for this machine)
   https://github.com/settings/ssh/new
