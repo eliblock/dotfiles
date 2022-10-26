3ffb13e0a845158c61d68d9c5914417cde460a27
Eli Block <3347571+eliblock@users.noreply.github.com>
2022-10-25 18:26:00 -0700

    bump default ruby to `3.1.2` 💎

--- ruby/install.darwin.sh
+++ ruby/install.darwin.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR"
 
-DEFAULT_RUBY=3.1.0
+DEFAULT_RUBY=3.1.2
 
 echo "• installing ruby $DEFAULT_RUBY as default ruby..."
 ruby-install ruby "$DEFAULT_RUBY" --no-reinstall
