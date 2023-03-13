2fd23d8b172603d38bf290cadc54ae526b182b08
Eli Block <3347571+eliblock@users.noreply.github.com>
2023-03-13 13:17:34 -0700

    bump default python to `3.11` 🐍

--- python/install.darwin.sh
+++ python/install.darwin.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR"
 
-DEFAULT_PYTHON=3.10.0
+DEFAULT_PYTHON=3.11
 
 echo "• installing python $DEFAULT_PYTHON as default python..."
 pyenv install "$DEFAULT_PYTHON" --skip-existing
