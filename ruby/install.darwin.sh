14b1e68aea64157815ab46b51de51c73d8d369a5
Eli Block <3347571+eliblock@users.noreply.github.com>
2024-12-28 16:11:27 -0800

    prompt before installing ruby 💎

--- ruby/install.darwin.sh
+++ ruby/install.darwin.sh
@@ -4,10 +4,14 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR"
 
-DEFAULT_RUBY=3.1.2
+DEFAULT_RUBY=3.4.1
 
-echo "• installing ruby $DEFAULT_RUBY as default ruby..."
-ruby-install ruby "$DEFAULT_RUBY" --no-reinstall
+read -p "❓💎 install ruby $DEFAULT_RUBY as default ruby? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "• installing ruby $DEFAULT_RUBY as default ruby..."
+    ruby-install ruby "$DEFAULT_RUBY" --no-reinstall
 
-# chruby will pick up ~/.ruby-version as the default version of ruby to use
-echo "ruby-$DEFAULT_RUBY" > "$HOME/.ruby-version"
+    # chruby will pick up ~/.ruby-version as the default version of ruby to use
+    echo "ruby-$DEFAULT_RUBY" > "$HOME/.ruby-version"
+fi
